3b518cf5287a2210a261ae0dc72e2eeee4a7ddfe
lynn9106 <e94091071@gs.ncku.edu.tw>
2023-11-01 16:10:18 +0800

    v0.2 Edge Detection 3.3 complete

--- controller.py
+++ controller.py
@@ -22,6 +22,11 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui.btn1_CTransformation.clicked.connect(self.ColorTransformation)
         self.ui.btn1_CExtraction.clicked.connect(self.ColorExtraction)
         self.ui.btn2_Gaussian.clicked.connect(self.GaussianBlur)
+        self.ui.btn2_Bilateral.clicked.connect(self.BilateralFilter)
+        self.ui.btn2_Median.clicked.connect(self.MedianFilter)
+        self.ui.btn3_SobelX.clicked.connect(self.SobelX)
+        self.ui.btn3_SobelY.clicked.connect(self.SobelY)
+        self.ui.btn3_CombAndThres.clicked.connect(self.CombAndThres)
 
 
     def openfile1(self):
@@ -86,7 +91,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         cv2.destroyAllWindows()
 
 # 2. Image Smoothing
-
     m = 1
     def GaussianBlur(self):
         if self.Image is None:
@@ -101,7 +105,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
     def update_gaussian_blur(self, value):
         self.m = value
         if self.Image is not None:
@@ -110,20 +113,154 @@ class MainWindow_controller(QtWidgets.QMainWindow):
             blur_image = cv2.GaussianBlur(image, (window_radius, window_radius), 0)
             cv2.imshow('gaussian blur', blur_image)
 
-# WHY INITIAL IS 0 ??????????
+    def BilateralFilter(self):
+        if self.Image is None:
+            return
+        image = cv2.imread(self.Image)
+        cv2.imshow('bilateral filter', image)
 
+        cv2.createTrackbar('m', 'bilateral filter', 1, 5, self.update_bilateral_filter)
+        cv2.setTrackbarMin('m', 'bilateral filter', 1)  # Set the minimum value
+        cv2.setTrackbarMax('m', 'bilateral filter', 5)  # Set the maximum value
+        cv2.setTrackbarPos('m', 'bilateral filter', 1)
 
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    def update_bilateral_filter(self, value):
+        self.m = value
+        if self.Image is not None:
+            image = cv2.imread(self.Image)
+            window_radius = 2 * self.m + 1
+            filter_image = cv2.bilateralFilter(image, window_radius,90,90)
+            cv2.imshow('bilateral filter', filter_image)
 
-'''
-    def window_radius(image):
-        global gaussian_m
-        image = cv2.imread(image)
-        window_radius = 2 * gaussian_m + 1
-        blur_image = cv2.GaussianBlur(image, (window_radius,window_radius), 0)
-        cv2.imshow('gaussian blur', blur_image)
+    def MedianFilter(self):
+        if self.Image is None:
+            return
+        image = cv2.imread(self.Image)
+        cv2.imshow('median filter', image)
 
-'''
+        cv2.createTrackbar('m', 'median filter', 1, 5, self.update_median_filter)
+        cv2.setTrackbarMin('m', 'median filter', 1)  # Set the minimum value
+        cv2.setTrackbarMax('m', 'median filter', 5)  # Set the maximum value
+        cv2.setTrackbarPos('m', 'median filter', 1)
 
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def update_median_filter(self,value):
+        self.m = value
+        if self.Image is not None:
+            image = cv2.imread(self.Image)
+            window_radius = 2 * self.m + 1
+            filter_image = cv2.medianBlur(image, window_radius)
+            cv2.imshow('median filter', filter_image)
+
+# 3. Edge Detection
+
+    def SobelX(self):
+        if self.Image is None:
+            return
+        image = cv2.imread(self.Image)
+        # Sobel x operator for edge detection
+        sobel_x_filter = np.array([[-1, 0, 1],
+                                   [-2, 0, 2],
+                                   [-1, 0, 1]])
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        smooth_image = cv2.GaussianBlur(gray_image, (3, 3), 0)
+
+        # Create an empty output image
+        sobel_x_image = np.zeros_like(smooth_image)
+        # Iterate through the image and apply the Sobel x operator
+        for y in range(1, smooth_image.shape[0] - 1):
+            for x in range(1, smooth_image.shape[1] - 1):
+                sobel_x_pixel = np.sum(smooth_image[y - 1:y + 2, x - 1:x + 2] * sobel_x_filter)
+                sobel_x_image[y, x] = np.abs(sobel_x_pixel) #the pixel value at position (y,x)
+
+        # normalize between [0,255]
+        min_value = np.min(sobel_x_image)
+        max_value = np.max(sobel_x_image)
+        sobel_x_image = np.uint8(255 * (sobel_x_image / (max_value - min_value)))
+
+        # Display the Sobel x image
+        cv2.imshow('Sobel X Image', sobel_x_image)
+
+        # Wait for a key press and close all windows
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def SobelY(self):
+        if self.Image is None:
+            return
+        image = cv2.imread(self.Image)
+        # Sobel y operator for edge detection
+        sobel_y_filter = np.array([[-1, -2, -1],
+                                   [0, 0, 0],
+                                   [1, 2, 1]])
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        smooth_image = cv2.GaussianBlur(gray_image, (3, 3), 0)
+
+        # Create an empty output image
+        sobel_y_image = np.zeros_like(smooth_image)
+        # Iterate through the image and apply the Sobel y operator
+        for y in range(1, smooth_image.shape[0] - 1):
+            for x in range(1, smooth_image.shape[1] - 1):
+                sobel_y_pixel = np.sum(smooth_image[y - 1:y + 2, x - 1:x + 2] * sobel_y_filter)
+                sobel_y_image[y, x] = np.abs(sobel_y_pixel)  # the pixel value at position (y,x)
+
+        # normalize between [0,255]
+        min_value = np.min(sobel_y_image)
+        max_value = np.max(sobel_y_image)
+        sobel_y_image = np.uint8(255 * (sobel_y_image / (max_value - min_value)))
+
+        # Display the Sobel x image
+        cv2.imshow('Sobel Y Image', sobel_y_image)
+
+        # Wait for a key press and close all windows
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def CombAndThres(self):
+        if self.Image is None:
+            return
+        image = cv2.imread(self.Image)
+        sobel_x_filter = np.array([[-1, 0, 1],
+                                   [-2, 0, 2],
+                                   [-1, 0, 1]])
+        sobel_y_filter = np.array([[-1, -2, -1],
+                                   [0, 0, 0],
+                                   [1, 2, 1]])
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        smooth_image = cv2.GaussianBlur(gray_image, (3, 3), 0)
+
+        # Create an empty output image
+        sobel_image = np.zeros_like(smooth_image)
+        # Iterate through the image and apply the Sobel y operator
+        for y in range(1, smooth_image.shape[0] - 1):
+            for x in range(1, smooth_image.shape[1] - 1):
+                sobel_x_pixel = np.sum(smooth_image[y - 1:y + 2, x - 1:x + 2] * sobel_x_filter)
+                sobel_y_pixel = np.sum(smooth_image[y - 1:y + 2, x - 1:x + 2] * sobel_y_filter)
+                sobel_image[y, x] = np.sqrt(np.square(sobel_x_pixel)+np.square(sobel_y_pixel))  # the pixel value at position (y,x)
+
+        # normalize between [0,255]
+        min_value = np.min(sobel_image)
+        max_value = np.max(sobel_image)
+        sobel_image = np.uint8(255 * (sobel_image / (max_value - min_value)))
+
+        # Display the Combination image
+        cv2.imshow('Combination of Sobel x and Sobel y', sobel_image)
+
+        threshold = 128
+        ret, thres_image = cv2.threshold(sobel_image, threshold, 255, cv2.THRESH_BINARY)    # 0, if lower than 128
+        cv2.imshow('Threshold result', thres_image)
+
+        # Wait for a key press and close all windows
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def GradientAngle(self):
+        if self.Image is None:
+            return
 
 
 
--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 
 # Load the image
-image = cv2.imread(r'C:\Users\lynn9106\PycharmProjects\OpenCV_HW1\Q1_image\rgb.jpg')
+image = cv2.imread(r'C:\Users\lynn9106\PycharmProjects\OpenCV_HW1\Q3_image\building.jpg')
+
+'''
 zeros = np.zeros(image.shape[:2], dtype = "uint8")
 # Split the BGR channels
 blue_channel, green_channel, red_channel = cv2.split(image)
@@ -18,5 +20,50 @@ cv2.imshow('R Channel', R_image)
 # Wait for a key press and close all windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+'''
+
+
+def sobel_edge_detection(self):
+    # Define the Sobel operators
+    Gx = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    Gy = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+
+    if self.Image is None:
+        return
+    image = cv2.imread(self.Image)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    smooth_image = cv2.GaussianBlur(gray_image, (3, 3), 0)
+
+    # Get the dimensions of the image
+    height, width = smooth_image.shape
+    # Create an output image with the same dimensions
+    out_image = np.zeros((height, width), dtype=np.int32)
+
+    for i in range(1, height - 1):
+        for j in range(1, width - 1):
+            if i == 0 or i == height - 1 or j == 0 or j == width - 1:
+                out_image[i, j] = 0
+            else:
+                X = 0
+                Y = 0
+                for k in range(-1, 2):
+                    for l in range(-1, 2):
+                        X += Gx[1 + k, 1 + l] * np.int32(smooth_image[i + k, j + l])
+                        Y += Gy[1 + k, 1 + l] * np.int32(smooth_image[i + k, j + l])
+                out_image[i, j] = np.abs(X) + np.abs(Y)
+
+    # Normalize the output image to the range [0, 255]
+    max_value = np.max(out_image)
+    min_value = np.min(out_image)
+    out_image = np.uint8(np.round(out_image / (max_value - min_value) * 255))
+
+    # Display the Sobel x image
+    cv2.imshow('Sobel X Image', out_image)
+
+    # Wait for a key press and close all windows
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
